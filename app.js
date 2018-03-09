8c6c881297b965f8ba13c6b6f5c336e736f8d5e9
GwonSeongJu <nayagwon@naver.com>
2018-03-09 14:46:30 +0900

    test

---
+++ app.js
@@ -0,0 +1,28 @@
+var express  = require('express');
+var app      = express();
+var path     = require('path');
+var mongoose = require('mongoose');
+var session  = require('express-session');
+
+// database
+mongoose.connect(process.env.PZGS_MONGO_DB);
+
+var db = mongoose.connection;
+db.once("open",function () {
+  console.log("DB connected!");
+});
+db.on("error",function (err) {
+  console.log("DB ERROR :", err);
+});
+
+// routes
+/*
+app.use('/', require('./routes/home'));
+app.use('/users', require('./routes/users'));
+app.use('/posts', require('./routes/posts'));
+*/
+// start server
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Server On!');
+});
