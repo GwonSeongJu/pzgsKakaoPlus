b259fb9c0c524b9d92fcf52c6f0d88d0f0b8299f
GwonSeongJu <nayagwon@naver.com>
2018-03-09 16:23:17 +0900

    test

--- routes/manage.js
+++ routes/manage.js
@@ -1,3 +1,9 @@
+var express  = require('express');
+var app      = express();
+var path     = require('path');
+var mongoose = require('mongoose');
+var session  = require('express-session');
+
 router.get('/', function (req,res) {
-  res.render("../posts/index.html");
+  res.render("./posts/index.html");
 });
